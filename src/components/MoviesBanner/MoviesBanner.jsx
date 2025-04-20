b075addeb4a7ba6b979f436ff16e2219dc770dfe
basesnel <basesnel@gmail.com>
2025-04-20 12:29:17 +0300

    Added Image-component into Banner-component.

--- src/components/MoviesBanner/MoviesBanner.jsx
+++ src/components/MoviesBanner/MoviesBanner.jsx
@@ -1,14 +1,16 @@
 import formatTimeAgo from "../../helpers/formatTimeAgo";
+import Image from "../Image/Image";
 
 import styles from "./styles.module.css";
 
 const MoviesBanner = ({ item }) => {
   return (
     <div className={styles.banner}>
-      <h3 className={styles.title}>{item.title}</h3>
+      <Image image={item?.image} />
+      {/* <h3 className={styles.title}>{item.title}</h3>
       <p className={styles.extra}>
         {formatTimeAgo(item.published)} by {item.author}
-      </p>
+      </p> */}
     </div>
   );
 };
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,7 +1,12 @@
+import MoviesBanner from "../../components/MoviesBanner/MoviesBanner";
 import styles from "./styles.module.css";
 
 const Main = () => {
-  return <main className={styles.main}>main</main>;
+  return (
+    <main className={styles.main}>
+      <MoviesBanner />
+    </main>
+  );
 };
 
 export default Main;
