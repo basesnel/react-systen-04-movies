e1c97d262d7ffcbde93749a23ec2e360ce858519
basesnel <basesnel@gmail.com>
2025-04-21 19:17:26 +0300

    Added MoviesItem component.

---
+++ src/components/MoviesItem/MoviesItem.jsx
@@ -0,0 +1,23 @@
+import formatTimeAgo from "../../helpers/formatTimeAgo";
+import styles from "./styles.module.css";
+
+const MoviesItem = ({ item }) => {
+  return (
+    <li className={styles.item}>
+      <div
+        className={styles.wrapper}
+        style={{
+          backgroundImage: `url(https://image.tmdb.org/t/p/w300${item?.poster_path})`,
+        }}
+      ></div>
+      <div className={styles.info}>
+        <h3 className={styles.title}>{item.title}</h3>
+        <p className={styles.extra}>
+          {`released ${formatTimeAgo(item.release_date)} | ${item.vote_range}`}
+        </p>
+      </div>
+    </li>
+  );
+};
+
+export default MoviesItem;
--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,11 @@
+import MoviesItem from "../MoviesItem/MoviesItem";
 import styles from "./styles.module.css";
 
 const MoviesList = ({ movies }) => {
   return (
     <ul className={styles.list}>
       {movies.map((item) => {
-        return <li key={item.id}>{item.title}</li>;
+        return <MoviesItem key={item.id} item={item} />;
       })}
     </ul>
   );
