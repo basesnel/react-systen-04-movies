b1c0615e33e88fdb8845ba4ee7698b6c00ca92ed
basesnel <basesnel@gmail.com>
2025-04-21 15:26:39 +0300

    Got first image succesfully.

--- src/components/MoviesBanner/MoviesBanner.jsx
+++ src/components/MoviesBanner/MoviesBanner.jsx
@@ -6,7 +6,7 @@ import styles from "./styles.module.css";
 const MoviesBanner = ({ item }) => {
   return (
     <div className={styles.banner}>
-      <Image image={item?.image} />
+      <Image image={`https://image.tmdb.org/t/p/w300${item?.poster_path}`} />
       <h3 className={styles.title}>{item.title}</h3>
       <p className={styles.extra}>
         {`released ${formatTimeAgo(item.release_date)} | ${item.vote_range}`}
