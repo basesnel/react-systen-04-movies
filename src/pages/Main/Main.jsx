e1b2a18191cb66d430af12375c7478fbe0ea1fc1
basesnel <basesnel@gmail.com>
2025-04-22 12:08:37 +0300

    Apply Skeleton in the App.

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -3,17 +3,21 @@ import { useEffect } from "react";
 import { getMovies } from "../../api/apiMovies";
 import MoviesBanner from "../../components/MoviesBanner/MoviesBanner";
 import MoviesList from "../../components/MoviesList/MoviesList";
+import Skeleton from "../../components/Skeleton/Skeleton";
 
 import styles from "./styles.module.css";
 
 const Main = () => {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchMovies = async () => {
       try {
+        setIsLoading(true);
         const response = await getMovies();
-        console.log(response);
+        // console.log(response);
+        setIsLoading(false);
         setMovies(response.results);
       } catch (error) {
         console.error(error);
@@ -25,8 +29,17 @@ const Main = () => {
 
   return (
     <main className={styles.main}>
-      {movies.length > 0 ? <MoviesBanner item={movies[0]} /> : null}
-      <MoviesList movies={movies} />
+      {movies.length > 0 && !isLoading ? (
+        <MoviesBanner item={movies[0]} />
+      ) : (
+        <Skeleton type="banner" count={1} />
+      )}
+
+      {!isLoading ? (
+        <MoviesList movies={movies} />
+      ) : (
+        <Skeleton type="item" count={20} />
+      )}
     </main>
   );
 };
