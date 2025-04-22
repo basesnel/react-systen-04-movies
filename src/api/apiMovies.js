09e5063ddc8a38b7c857abadb7673144c191c435
basesnel <basesnel@gmail.com>
2025-04-22 09:06:43 +0300

    Added handleError function to ipiMovies.

--- src/api/apiMovies.js
+++ src/api/apiMovies.js
@@ -3,6 +3,22 @@ import axios from "axios";
 const API_BASE_URL = import.meta.env.VITE_MOVIES_API_BASE_URL;
 const API_KEY = import.meta.env.VITE_MOVIES_API_KEY;
 
+const handleError = (error) => {
+  if (error.response) {
+    const { data, status, headers } = error.response;
+    console.log(data);
+    console.log(status);
+    console.log(headers);
+  } else if (error.request) {
+    const { request } = error;
+    console.log(request);
+  } else {
+    const { message } = error;
+    console.log("Error", message);
+  }
+  console.log(error.config);
+};
+
 const getMovies = async () => {
   const options = {
     url: `${API_BASE_URL}movie/popular`,
@@ -20,7 +36,7 @@ const getMovies = async () => {
   return axios
     .request(options)
     .then((res) => res.data)
-    .catch((err) => console.error(err));
+    .catch(handleError);
 };
 
 export default getMovies;
