4451b0f7ee94d42cb19786a581805cabe54cab34
basesnel <basesnel@gmail.com>
2025-04-22 13:18:07 +0300

    Changed items description.

--- src/components/MoviesBanner/MoviesBanner.jsx
+++ src/components/MoviesBanner/MoviesBanner.jsx
@@ -9,7 +9,9 @@ const MoviesBanner = ({ item }) => {
       <Image image={`https://image.tmdb.org/t/p/w300${item?.poster_path}`} />
       <h3 className={styles.title}>{item.title}</h3>
       <p className={styles.extra}>
-        {`released ${formatTimeAgo(item.release_date)} | ${item.vote_average}`}
+        {`Release: ${item.release_date} (${formatTimeAgo(
+          item.release_date
+        )}) | ${item.vote_average}`}
       </p>
     </div>
   );
