00bbf27e2a04def9882ec11f0697de849bfcca0e
basesnel <basesnel@gmail.com>
2025-04-24 16:53:02 +0300

    Renamed secondPast to SecondsDiff in the formatTimeAgo function.

--- src/helpers/formatTimeAgo.js
+++ src/helpers/formatTimeAgo.js
@@ -1,7 +1,7 @@
 const formatTimeAgo = (dateString) => {
   const now = new Date();
   const date = new Date(dateString);
-  const secondPast = (now.getTime() - date.getTime()) / 1000;
+  const secondsDiff = (now.getTime() - date.getTime()) / 1000;
 
   const secondsInMinute = 60;
   const secondsInHour = secondsInMinute * 60;
@@ -11,36 +11,36 @@ const formatTimeAgo = (dateString) => {
   const secondsInYear = Math.round(secondsInDay * 365.25);
   const secondsInTwoYear = secondsInYear * 2;
 
-  if (secondPast < secondsInMinute) {
-    return `${Math.floor(secondPast)}s ago`;
+  if (secondsDiff < secondsInMinute) {
+    return `${Math.floor(secondsDiff)}s ago`;
   }
 
-  if (secondPast < secondsInHour) {
-    return `${Math.floor(secondPast / secondsInMinute)}m ago`;
+  if (secondsDiff < secondsInHour) {
+    return `${Math.floor(secondsDiff / secondsInMinute)}m ago`;
   }
 
-  if (secondPast < secondsInDay) {
-    return `${Math.floor(secondPast / secondsInHour)}h ago`;
+  if (secondsDiff < secondsInDay) {
+    return `${Math.floor(secondsDiff / secondsInHour)}h ago`;
   }
 
-  if (secondPast < secondsInWeek) {
-    const day = Math.floor(secondPast / secondsInDay);
+  if (secondsDiff < secondsInWeek) {
+    const day = Math.floor(secondsDiff / secondsInDay);
     return day === 1 ? `${day} day ago` : `${day} days ago`;
   }
 
-  if (secondPast < secondsInMonth) {
-    const week = Math.floor(secondPast / secondsInWeek);
+  if (secondsDiff < secondsInMonth) {
+    const week = Math.floor(secondsDiff / secondsInWeek);
     return week === 1 ? `${week} week ago` : `${week} weeks ago`;
   }
 
-  if (secondPast < secondsInYear) {
-    const month = Math.floor(secondPast / secondsInMonth);
+  if (secondsDiff < secondsInYear) {
+    const month = Math.floor(secondsDiff / secondsInMonth);
     return month === 1 ? `${month} month ago` : `${month} months ago`;
   }
 
-  if (secondPast < secondsInTwoYear) {
-    const year = Math.floor(secondPast / secondsInYear);
-    const month = Math.floor((secondPast % secondsInYear) / secondsInMonth);
+  if (secondsDiff < secondsInTwoYear) {
+    const year = Math.floor(secondsDiff / secondsInYear);
+    const month = Math.floor((secondsDiff % secondsInYear) / secondsInMonth);
 
     if (!month) return `${year} year ago`;
     if (month === 1) return `${year} year and month ago`;
@@ -48,8 +48,8 @@ const formatTimeAgo = (dateString) => {
     return `${year} year and ${month} months ago`;
   }
 
-  if (secondPast >= secondsInTwoYear) {
-    const year = Math.floor(secondPast / secondsInYear);
+  if (secondsDiff >= secondsInTwoYear) {
+    const year = Math.floor(secondsDiff / secondsInYear);
     return year === 1 ? `${year} year ago` : `${year} years ago`;
   }
 };
