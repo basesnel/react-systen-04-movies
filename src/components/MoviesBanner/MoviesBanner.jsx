e351bed42988b756072c4e7f83de2c1a5501ea1f
basesnel <basesnel@gmail.com>
2025-04-25 18:05:08 +0300

    Added other tests for function FormatTimeAgo.

--- src/components/MoviesBanner/MoviesBanner.jsx
+++ src/components/MoviesBanner/MoviesBanner.jsx
@@ -8,6 +8,10 @@ const MoviesBanner = ({ item }) => {
   const twoYearsAgo = "2023-04-25";
   const oneYearAndMonths = "2023-06-20";
   const oneYearAndMonth = "2024-03-20";
+  const oneYear = "2024-04-20";
+  const months = "2024-05-30";
+  const month = "2025-03-25";
+  const weeks = "2025-03-27";
 
   return (
     <div className={styles.banner}>
@@ -22,6 +26,10 @@ const MoviesBanner = ({ item }) => {
       <p>{`${twoYearsAgo} (${formatTimeAgo(twoYearsAgo)})`}</p>
       <p>{`${oneYearAndMonths} (${formatTimeAgo(oneYearAndMonths)})`}</p>
       <p>{`${oneYearAndMonth} (${formatTimeAgo(oneYearAndMonth)})`}</p>
+      <p>{`${oneYear} (${formatTimeAgo(oneYear)})`}</p>
+      <p>{`${months} (${formatTimeAgo(months)})`}</p>
+      <p>{`${month} (${formatTimeAgo(month)})`}</p>
+      <p>{`${weeks} (${formatTimeAgo(weeks)})`}</p>
     </div>
   );
 };
