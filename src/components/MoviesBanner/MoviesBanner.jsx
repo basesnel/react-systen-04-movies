ab5bc5e6754de222b9ce930d49d987d3dc95295a
basesnel <basesnel@gmail.com>
2025-04-25 17:58:49 +0300

    Added test for function FormatTimeAgo.

--- src/components/MoviesBanner/MoviesBanner.jsx
+++ src/components/MoviesBanner/MoviesBanner.jsx
@@ -4,6 +4,11 @@ import Image from "../Image/Image";
 import styles from "./styles.module.css";
 
 const MoviesBanner = ({ item }) => {
+  const threeYearsAgo = "2022-04-25";
+  const twoYearsAgo = "2023-04-25";
+  const oneYearAndMonths = "2023-06-20";
+  const oneYearAndMonth = "2024-03-20";
+
   return (
     <div className={styles.banner}>
       <Image image={`https://image.tmdb.org/t/p/w300${item?.poster_path}`} />
@@ -13,6 +18,10 @@ const MoviesBanner = ({ item }) => {
           item.release_date
         )}) | ${item.vote_average}`}
       </p>
+      <p>{`${threeYearsAgo} (${formatTimeAgo(threeYearsAgo)})`}</p>
+      <p>{`${twoYearsAgo} (${formatTimeAgo(twoYearsAgo)})`}</p>
+      <p>{`${oneYearAndMonths} (${formatTimeAgo(oneYearAndMonths)})`}</p>
+      <p>{`${oneYearAndMonth} (${formatTimeAgo(oneYearAndMonth)})`}</p>
     </div>
   );
 };
