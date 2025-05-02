32c97a2fc74a2f69977dfb2235d85071a7f1d2f4
basesnel <basesnel@gmail.com>
2025-05-02 20:15:34 +0300

    Changed main poster on MovieBanner

--- src/components/MoviesBanner/MoviesBanner.jsx
+++ src/components/MoviesBanner/MoviesBanner.jsx
@@ -7,7 +7,7 @@ import styles from "./styles.module.css";
 const MoviesBanner = ({ item }) => {
   return (
     <div className={styles.banner}>
-      <Image image={`https://image.tmdb.org/t/p/w300${item?.poster_path}`} />
+      <Image image={`https://image.tmdb.org/t/p/w500${item?.poster_path}`} />
       <h3 className={styles.title}>{item.title}</h3>
       <p className={styles.extra}>
         {`Release: ${item.release_date} | (${formatTimeDiff(
