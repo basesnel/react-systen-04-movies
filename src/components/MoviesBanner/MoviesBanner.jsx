9d90a6bd20f07e11d886603765911a754e8c1e21
basesnel <basesnel@gmail.com>
2025-05-06 19:02:45 +0300

    Highlighted film's indicators in the BannerMovie.

--- src/components/MoviesBanner/MoviesBanner.jsx
+++ src/components/MoviesBanner/MoviesBanner.jsx
@@ -10,11 +10,22 @@ const MoviesBanner = ({ item }) => {
       <Image image={`https://image.tmdb.org/t/p/w500${item?.poster_path}`} />
       <h3 className={styles.title}>{item.title}</h3>
       <p className={styles.extra}>
-        {`Release: ${item.release_date} | (${formatTimeDiff(
+        {"Release: "}
+        <span className={styles.highlighted}>{`${item.release_date}`}</span>
+        {" | "}
+        <span className={styles.highlighted}>{`(${formatTimeDiff(
           item.release_date
-        )}) | score: ${formatRoundToTwo(item.vote_average)} (votes: ${
-          item.vote_count
-        }) | popularity: ${formatRoundToTwo(item.popularity)}`}
+        )})`}</span>
+        {" | score: "}
+        <span className={styles.highlighted}>{`${formatRoundToTwo(
+          item.vote_average
+        )}`}</span>
+        {" (votes: "}
+        <span className={styles.highlighted}>{`${item.vote_count})`}</span>
+        {" | popularity: "}
+        <span className={styles.highlighted}>{`${formatRoundToTwo(
+          item.popularity
+        )}`}</span>
       </p>
     </div>
   );
