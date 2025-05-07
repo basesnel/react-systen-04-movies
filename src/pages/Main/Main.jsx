cebce211ed82ef8436522bea096202d927c6e7ff
basesnel <basesnel@gmail.com>
2025-05-07 21:05:17 +0300

    Deleted comments of Skeleton from Main.jsx.

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -11,7 +11,6 @@ import MoviesBanner from "../../components/MoviesBanner/MoviesBanner";
 import MoviesList from "../../components/MoviesList/MoviesList";
 import Pagination from "../../components/Pagination/Pagination";
 import Search from "../../components/Search/Search";
-import Skeleton from "../../components/Skeleton/Skeleton";
 import { TOTAL_PAGES } from "../../constants/constants";
 import useDebounce from "../../helpers/hooks/useDebounce";
 
@@ -101,12 +100,6 @@ const Main = () => {
         item={movies.length > 0 && movies[0]}
       />
 
-      {/* {movies.length > 0 && !isLoading ? (
-        <MoviesBanner item={movies[0]} />
-      ) : (
-        <Skeleton type="banner" count={1} />
-      )} */}
-
       <Pagination
         handlePreviousPage={handlePreviousPage}
         handleNextPage={handleNextPage}
@@ -116,12 +109,6 @@ const Main = () => {
       />
 
       <MoviesList isLoading={isLoading} movies={movies} />
-
-      {/* {!isLoading ? (
-        <MoviesList movies={movies} />
-      ) : (
-        <Skeleton type="item" count={20} />
-      )} */}
 
       <Pagination
         handlePreviousPage={handlePreviousPage}
