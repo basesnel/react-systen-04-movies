05cdc032ab56cde503e1f930e739e06605543acf
basesnel <basesnel@gmail.com>
2025-05-09 19:47:10 +0300

    Added component LatestMovies.

---
+++ src/components/LatestMovies/LatestMovies.jsx
@@ -0,0 +1,9 @@
+import formatDate from "../../helpers/formatDate";
+
+import styles from "./styles.module.css";
+
+const LatestMovies = () => {
+  return <section className={styles.section}></section>;
+};
+
+export default LatestMovies;
