f3093336cefb3d8f38ad7f0dc36d315266cbfd95
basesnel <basesnel@gmail.com>
2025-05-09 21:44:24 +0300

    Clear up Main-page component.

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,17 +1,10 @@
 import {
   getMovies,
-  getMovieGenres,
   getDiscoveryMovies,
   getFoundMovies,
 } from "../../api/apiMovies";
-import Genres from "../../components/Genres/Genres";
 import LatestMovies from "../../components/LatestMovies/LatestMovies";
-import MoviesBanner from "../../components/MoviesBanner/MoviesBanner";
 import MoviesByFilters from "../../components/MoviesByFilters/MoviesByFilters";
-import MoviesList from "../../components/MoviesList/MoviesList";
-import Pagination from "../../components/Pagination/Pagination";
-import Search from "../../components/Search/Search";
-import { TOTAL_PAGES } from "../../constants/constants";
 import useDebounce from "../../helpers/hooks/useDebounce";
 import useFetch from "../../helpers/hooks/useFetch";
 import useFilters from "../../helpers/hooks/useFilters";
@@ -52,23 +45,6 @@ const Main = () => {
   });
 
   const { data, error, isLoading } = useFetch(getFunction, params);
-  // const { data: dataGenres } = useFetch(getMovieGenres);
-
-  // const handleNextPage = () => {
-  //   if (filters.page < TOTAL_PAGES) {
-  //     changeFilter("page", filters.page + 1);
-  //   }
-  // };
-
-  // const handlePreviousPage = () => {
-  //   if (filters.page > 1) {
-  //     changeFilter("page", filters.page - 1);
-  //   }
-  // };
-
-  // const handlePageClick = (pageNumber) => {
-  //   changeFilter("page", pageNumber);
-  // };
 
   return (
     <main className={styles.main}>
@@ -80,37 +56,6 @@ const Main = () => {
         filters={filters}
         changeFilter={changeFilter}
       />
-
-      {/* {dataGenres ? (
-        <Genres
-          genres={dataGenres.genres}
-          selectedGenre={filters.with_genres}
-          setSelectedGenre={(genre) => changeFilter("with_genres", genre)}
-        />
-      ) : null}
-
-      <Search
-        query={filters.query}
-        setQuery={(query) => changeFilter("query", query)}
-      />
-
-      <Pagination
-        handlePreviousPage={handlePreviousPage}
-        handleNextPage={handleNextPage}
-        handlePageClick={handlePageClick}
-        totalPages={TOTAL_PAGES}
-        currentPage={filters.page}
-      />
-
-      <MoviesList isLoading={isLoading} movies={data?.results} />
-
-      <Pagination
-        handlePreviousPage={handlePreviousPage}
-        handleNextPage={handleNextPage}
-        handlePageClick={handlePageClick}
-        totalPages={TOTAL_PAGES}
-        currentPage={filters.page}
-      /> */}
     </main>
   );
 };
