bb051664163c788c9974f4ce736fd5e51957d4b4
basesnel <basesnel@gmail.com>
2025-05-10 16:05:54 +0300

    Clear up code of MoviesByFilters component and changed style of MovieFilters.

--- src/components/MoviesByFilters/MoviesByFilters.jsx
+++ src/components/MoviesByFilters/MoviesByFilters.jsx
@@ -1,16 +1,10 @@
-// import { getMovieGenres } from "../../api/apiMovies";
 import { TOTAL_PAGES } from "../../constants/constants";
 import MoviesFilters from "../MoviesFilters/MoviesFilters";
-// import useFetch from "../../helpers/hooks/useFetch";
-// import Genres from "../Genres/Genres";
 import MoviesList from "../MoviesList/MoviesList";
 import Pagination from "../Pagination/Pagination";
-// import Search from "../Search/Search";
 import styles from "./styles.module.css";
 
 const MoviesByFilters = ({ filters, changeFilter, isLoading, movies }) => {
-  // const { data: dataGenres } = useFetch(getMovieGenres);
-
   const handleNextPage = () => {
     if (filters.page < TOTAL_PAGES) {
       changeFilter("page", filters.page + 1);
@@ -30,18 +24,6 @@ const MoviesByFilters = ({ filters, changeFilter, isLoading, movies }) => {
   return (
     <section className={styles.section}>
       <MoviesFilters filters={filters} changeFilter={changeFilter} />
-      {/* {dataGenres ? (
-        <Genres
-          genres={dataGenres.genres}
-          selectedGenre={filters.with_genres}
-          setSelectedGenre={(genre) => changeFilter("with_genres", genre)}
-        />
-      ) : null}
-
-      <Search
-        query={filters.query}
-        setQuery={(query) => changeFilter("query", query)}
-      /> */}
 
       <Pagination
         handlePreviousPage={handlePreviousPage}
