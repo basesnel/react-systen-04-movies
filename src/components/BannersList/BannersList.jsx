31863705bd344af6b534c99fd7c894ac40bbe40e
basesnel <basesnel@gmail.com>
2025-05-12 15:18:24 +0300

    Check markup in the validator.

--- src/components/BannersList/BannersList.jsx
+++ src/components/BannersList/BannersList.jsx
@@ -7,7 +7,11 @@ const BannersList = ({ banners }) => {
   return (
     <ul className={styles.banners}>
       {banners?.map((banner) => {
-        return <MoviesBanner key={banner.id} item={banner} />;
+        return (
+          <li key={banner.id}>
+            <MoviesBanner item={banner} />
+          </li>
+        );
       })}
     </ul>
   );
