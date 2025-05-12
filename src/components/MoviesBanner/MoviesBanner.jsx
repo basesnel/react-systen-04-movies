22ebfd00aa839454d46df4502df8d2c33c27c9d6
basesnel <basesnel@gmail.com>
2025-05-12 16:20:35 +0300

    Added div for wrap info in the Banner component.

--- src/components/MoviesBanner/MoviesBanner.jsx
+++ src/components/MoviesBanner/MoviesBanner.jsx
@@ -8,25 +8,27 @@ const MoviesBanner = ({ item }) => {
   return (
     <div className={styles.banner}>
       <Image image={`https://image.tmdb.org/t/p/w500${item?.poster_path}`} />
-      <h3 className={styles.title}>{item.title}</h3>
-      <p className={styles.extra}>
-        {"Release: "}
-        <span className={styles.highlighted}>{`${item.release_date}`}</span>
-        {" | "}
-        <span className={styles.highlighted}>{`(${formatTimeDiff(
-          item.release_date
-        )})`}</span>
-        {" | score: "}
-        <span className={styles.highlighted}>{`${formatRoundToTwo(
-          item.vote_average
-        )}`}</span>
-        {" (votes: "}
-        <span className={styles.highlighted}>{`${item.vote_count})`}</span>
-        {" | popularity: "}
-        <span className={styles.highlighted}>{`${formatRoundToTwo(
-          item.popularity
-        )}`}</span>
-      </p>
+      <div className={styles.info}>
+        <h3 className={styles.title}>{item.title}</h3>
+        <p className={styles.extra}>
+          {"Release: "}
+          <span className={styles.highlighted}>{`${item.release_date}`}</span>
+          {" | "}
+          <span className={styles.highlighted}>{`(${formatTimeDiff(
+            item.release_date
+          )})`}</span>
+          {" | score: "}
+          <span className={styles.highlighted}>{`${formatRoundToTwo(
+            item.vote_average
+          )}`}</span>
+          {" (votes: "}
+          <span className={styles.highlighted}>{`${item.vote_count})`}</span>
+          {" | popularity: "}
+          <span className={styles.highlighted}>{`${formatRoundToTwo(
+            item.popularity
+          )}`}</span>
+        </p>
+      </div>
     </div>
   );
 };
