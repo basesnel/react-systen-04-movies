93405cdf260071bb83bb78fd6684a81009309c6d
basesnel <basesnel@gmail.com>
2025-05-12 15:51:02 +0300

    Added hidden-title to MoviesByFilters section.

--- src/components/MoviesByFilters/MoviesByFilters.jsx
+++ src/components/MoviesByFilters/MoviesByFilters.jsx
@@ -23,6 +23,7 @@ const MoviesByFilters = ({ filters, changeFilter, isLoading, movies }) => {
 
   return (
     <section className={styles.section}>
+      <h2 className={styles.hidden}>discovery movie by filters</h2>
       <MoviesFilters filters={filters} changeFilter={changeFilter} />
 
       <Pagination
