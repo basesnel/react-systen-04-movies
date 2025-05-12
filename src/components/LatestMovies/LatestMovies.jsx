d647c6598c0a570e99973938d7a66fee0f0b3bd8
basesnel <basesnel@gmail.com>
2025-05-12 15:46:24 +0300

    Added hidden-title to latest-movies section.

--- src/components/LatestMovies/LatestMovies.jsx
+++ src/components/LatestMovies/LatestMovies.jsx
@@ -5,6 +5,7 @@ import styles from "./styles.module.css";
 const LatestMovies = ({ banners, isLoading }) => {
   return (
     <section className={styles.section}>
+      <h2 className={styles.hidden}>latest popular movies</h2>
       <BannersList banners={banners} isLoading={isLoading} />
     </section>
   );
