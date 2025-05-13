d9ed4cbd30d2cbf93d147809b6335e67e7a017a5
basesnel <basesnel@gmail.com>
2025-05-13 16:05:20 +0300

    Redesighned styles of LatestMovies component.

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,9 +1,5 @@
 import LatestMovies from "../../components/LatestMovies/LatestMovies";
 import MoviesByFilters from "../../components/MoviesByFilters/MoviesByFilters";
-// import useDebounce from "../../helpers/hooks/useDebounce";
-// import useFetch from "../../helpers/hooks/useFetch";
-// import useFilters from "../../helpers/hooks/useFilters";
-// import switchGet from "../../helpers/switchGet";
 
 import styles from "./styles.module.css";
 
@@ -11,7 +7,6 @@ const Main = () => {
   return (
     <main className={styles.main}>
       <LatestMovies />
-
       <MoviesByFilters />
     </main>
   );
