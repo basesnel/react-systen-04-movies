90c5360712db3a8af6bb2dd5b0e92ddfe3213b29
basesnel <basesnel@gmail.com>
2025-05-13 16:29:10 +0300

    Added pagination for LatestMovies also.

--- src/components/LatestMovies/LatestMovies.jsx
+++ src/components/LatestMovies/LatestMovies.jsx
@@ -1,17 +1,53 @@
+import { TOTAL_PAGES } from "../../constants/constants";
 import HiddenTitle from "../HiddenTitle/HiddenTitle";
 import BannersList from "../BannersList/BannersList";
+import PaginationWrapper from "../PaginationWrapper/PaginationWrapper";
+import useFilters from "../../helpers/hooks/useFilters";
 import useFetch from "../../helpers/hooks/useFetch";
 import { getNowPlayingMovies } from "../../api/apiMovies";
 
 import styles from "./styles.module.css";
 
 const LatestMovies = () => {
-  const params = { page: 1 };
+  const { filters, changeFilter } = useFilters({
+    page: 1,
+    query: "",
+    with_genres: null,
+  });
+
+  const params = { page: filters.page };
   const { data, isLoading } = useFetch(getNowPlayingMovies, params);
+
+  const handleNextPage = () => {
+    if (filters.page < TOTAL_PAGES) {
+      changeFilter("page", filters.page + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (filters.page > 1) {
+      changeFilter("page", filters.page - 1);
+    }
+  };
+
+  const handlePageClick = (pageNumber) => {
+    changeFilter("page", pageNumber);
+  };
+
   return (
     <section className={styles.section}>
       <HiddenTitle title="latest popular movies" />
-      <BannersList banners={data && data.results} isLoading={isLoading} />
+      <PaginationWrapper
+        top
+        bottom
+        handlePreviousPage={handlePreviousPage}
+        handleNextPage={handleNextPage}
+        handlePageClick={handlePageClick}
+        totalPages={TOTAL_PAGES}
+        currentPage={filters.page}
+      >
+        <BannersList banners={data && data.results} isLoading={isLoading} />
+      </PaginationWrapper>
     </section>
   );
 };
